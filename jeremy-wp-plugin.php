961860fb140dd70ccb69765436dc9c301e477f42
Jeremy Richardson <jeremy.richrdson@gmail.com>
2025-02-17 20:16:17 -0600

    feat: add plugin with ci step to deploy to Hetzner

---
+++ jeremy-wp-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Jeremy's Blog Plugin
+ * Plugin URI:  https://example.com
+ * Description: A place to customize WordPress when I need to
+ * Version:     1.0.0
+ * Author:      Jeremy Richardson
+ * Author URI:  https://jeremyrichardson.dev
+ * License:     GPL-2.0+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ 
